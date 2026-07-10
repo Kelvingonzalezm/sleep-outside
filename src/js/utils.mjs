1220d20e772de9d66178e9040f36c33a46c567df
Kelvin Gonzalez <kelvingonzalezm@gmail.com>
2026-07-10 14:12:49 -0400

    Complete dynamic product list

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -29,3 +29,20 @@ export function getParam(param) {
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 } 
+
+// render lis with template
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}

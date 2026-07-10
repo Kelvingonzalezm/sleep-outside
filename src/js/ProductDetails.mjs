f71daf949cd649811cd9abe35ecfe51186ceb1bf
Kelvin Gonzalez <kelvingonzalezm@gmail.com>
2026-07-09 20:49:23 -0400

    Create dynamic product details and cart functionality

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,45 @@
+export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails();
+        document
+            .querySelector('#addToCart')
+            .addEventListener('click', this.addProductToCart.bind(this));
+    }
+
+    addProductToCart() {
+        let cart = JSON.parse(localStorage.getItem('so-cart')) || [];
+
+        if (!Array.isArray(cart)) {
+            cart = []
+        }
+        
+        cart.push(this.product);
+
+        localStorage.setItem('so-cart', JSON.stringify(cart));
+
+        console.log('Product added to cart');
+    }
+
+    renderProductDetails() {
+        const container = document.querySelector('#product-detail')
+
+        container.innerHTML = `
+            <h3>${this.product.Brand.Name}</h3>
+            <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+            <img class="divider" src="${this.product.Image}" alt="${this.product.NameWithoutBrand}" />
+            <p class="product-card__price">$${this.product.FinalPrice}</p>
+            <p class="product__color">${this.product.Colors[0].ColorName}</p>
+            <p class="product__description">${this.product.DescriptionHtmlSimple}</p>
+            <div class="product-detail__add">
+                <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+            </div>    
+        `;
+    }
+}
--- src/js/product.js
+++ src/js/product.js
@@ -1,30 +1,11 @@
-import { setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+
+const productId = getParam('product');
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  let cart = JSON.parse(localStorage.getItem('so-cart')) || [];
+const product = new ProductDetails(productId, dataSource);
 
-  if (!Array.isArray(cart)) {
-    cart = [];
-  }
-
-  cart.push(product);
-  setLocalStorage('so-cart', cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  console.log('CLICK DETECTADO');
-  console.log('TARGET:', e.target);
-  console.log('DATASET ID:', e.target.dataset.id);
-
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+product.init();
